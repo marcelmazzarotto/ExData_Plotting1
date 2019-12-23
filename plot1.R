07a0a16c3e43828f88a00858967e40d9644e683f
Marcel <marcelmazzarotto@yahoo.com.br>
2019-12-23 05:21:30 +0100

    Scripts and Plots

---
+++ plot1.R
@@ -0,0 +1,24 @@
+
+## Extracting the file.
+if(file.exists("exdata_data_household_power_consumption.zip")) {
+  unzip("exdata_data_household_power_consumption.zip")
+  setwd("Project1")
+}
+
+library(data.table)
+
+## Reading the file.
+power_data = fread("household_power_consumption.txt", header=TRUE, sep=";",
+                   stringsAsFactors=FALSE, dec=".")
+
+## Subsetting the file.
+power_subdata = subset(power_data, power_data$Date == "1/2/2007" | power_data$Date == "2/2/2007")
+
+## Plot 1: Histogram "Global Active Power"
+png("plot1.png", width=480, height=480)  ## Start PNG file
+GlobalActivePower = as.numeric(as.character(power_subdata$Global_active_power))
+hist(GlobalActivePower, col = "red", main = "Global Active Power", ylab = "Frequency",
+     xlab = "Global Active Power (kilowatts)")
+
+dev.off() ## Close PNG file
+
---
+++ plot4.R
@@ -0,0 +1,56 @@
+## Extracting the file.
+if(file.exists("exdata_data_household_power_consumption.zip")) {
+  unzip("exdata_data_household_power_consumption.zip")
+  setwd("Project1")
+}
+
+library(data.table)
+
+## Reading the file.
+power_data = fread("household_power_consumption.txt", header=TRUE, sep=";",
+                   stringsAsFactors=FALSE, dec=".")
+
+## Subsetting the file.
+power_subdata = subset(power_data, power_data$Date == "1/2/2007" |
+                         power_data$Date == "2/2/2007")
+
+## Creating new variable that accounts for time in the day.
+DateTime = strptime(paste(power_subdata$Date, power_subdata$Time, sep = " "),
+                   "%d/%m/%Y %H:%M:%S")
+
+png("plot4.png", width=480, height=480) ## Start PNG file
+
+## Arranging plots in a 2x2 dimension.
+par(mfcol = c(2,2))
+
+## Introducing plot [1,1].
+GlobalActivePower = as.numeric(as.character(power_subdata$Global_active_power))
+plot(DateTime, GlobalActivePower, type="l",  xlab="", ylab = "Global Active Power (kilowatts)")
+
+## Introducing plot [2,1].
+## Converting variables to numeric format.
+Sub_Metering_1 = as.numeric(as.character(power_subdata$Sub_metering_1))
+Sub_Metering_2 = as.numeric(as.character(power_subdata$Sub_metering_2))
+Sub_Metering_3 = as.numeric(as.character(power_subdata$Sub_metering_3))
+ 
+## Plotting.
+plot(DateTime, Sub_Metering_1, xlab = "", ylab = "Energy sub metering", type ="l")
+
+## Annotating lines Met2 and Met3.
+lines(DateTime, Sub_Metering_2, type = "l", col = "red")
+lines(DateTime, Sub_Metering_3, type = "l", col = "blue")
+ 
+## Annotating legend
+legend("topright", c("Sub_Metering_1", "Sub_Metering_2", "Sub_Metering_3"),
+       col = c("black", "red", "blue"),cex = 0.2, lty= 1, lwd = 2.5)
+
+## Introducing plot [1,2].
+with(power_subdata, plot(DateTime, Voltage, xlab = "datetime", ylab = "Voltage", type = "l"))
+
+## Introducing plot [2,2].
+GlobalReactivePower = as.numeric(as.character(power_subdata$Global_reactive_power))
+with(power_subdata, plot(DateTime,GlobalReactivePower, xlab = "datetime", 
+                         ylab = "Global_reactive_power", type = "l"))
+
+
+dev.off()  ## Close PNG file
